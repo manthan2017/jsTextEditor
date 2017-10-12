b4874545c91b30ff9e85ef96872ecafa42d09fd9
DaniilOlshanskyi <danil2477@gmail.com>
2017-10-11 22:30:17 -0500

    132

--- script.js
+++ script.js
@@ -16,20 +16,25 @@ $(function(){
 	$('#code_itself').html(debug_code_snippet); //this will be from database
 	
 	var code_html=$('#code_itself').html();
-	for (i=0; i<code_html.length; i++){
-		if (code_html[i] == "{"){
-			var left_side = code_html.slice(0,i-1);
-			var right_side = code_html.slice(i+1, code_html.length);
-			var result = left_side + '<span class="red">{</span>' + right_side ;
-			$('#code_itself').html(result);
-			code_html=$('#code_itself').html();
-		} else if (code_html[i] == "}"){
-			var left_side = code_html.slice(0,i-1);
-			var right_side = code_html.slice(i+1, code_html.length);
-			var result = left_side + '<span class="red">}</span>' + right_side ;
-			$('#code_itself').html(result);
-			code_html=$('#code_itself').html();
+	
+	function highlight_brackets(){
+		for (i=0; i<code_html.length; i++){
+			if (code_html[i] == "{"){
+				var left_side = code_html.slice(0,i-1);
+				var right_side = code_html.slice(i+1, code_html.length);
+				var result = left_side + '<span class="red">{</span>' + right_side ;
+				$('#code_itself').html(result);
+				code_html=$('#code_itself').html();
+			} else if (code_html[i] == "}"){
+				var left_side = code_html.slice(0,i-1);
+				var right_side = code_html.slice(i+1, code_html.length);
+				var result = left_side + '<span class="red">}</span>' + right_side ;
+				$('#code_itself').html(result);
+				code_html=$('#code_itself').html();
+			}
 		}
 	}
-
+	highlight_brackets();
+	
+	$('#code_itself').on("keypress", function(){highlight_brackets()});
 });
