7e4c41ee7958cad44138ef1370070f9958869a36
DaniilOlshanskyi <danil2477@gmail.com>
2017-10-11 21:43:29 -0500

    123

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ $(function(){
 	
 //code goes here
 	
-};
+	var number_counter = 0;
+	var number_html = "";
+	for (i = 0; i < $('#line_numbers').height()/19; i++){
+		number_html += number_counter+"<br>";
+		number_counter++;
+	}		
+	$('#line_numbers').html(number_html);
+});
