e0b23faf8f83829def409074aab6cebb2f307b50
DaniilOlshanskyi <danil2477@gmail.com>
2017-10-11 23:12:12 -0500

    Added as much braces highliting as i could

--- script.js
+++ script.js
@@ -18,23 +18,28 @@ $(function(){
 	var code_html=$('#code_itself').html();
 	
 	function highlight_brackets(){
+		code_html=$('#code_itself').html();
 		for (i=0; i<code_html.length; i++){
-			if (code_html[i] == "{"){
+			if (code_html[i] == "{"){  
 				var left_side = code_html.slice(0,i-1);
 				var right_side = code_html.slice(i+1, code_html.length);
-				var result = left_side + '<span class="red">{</span>' + right_side ;
+				//var result = left_side + '<span class="red" id="span' + i +'">{</span>' + right_side ;
+				var result = left_side + '<span class="red" class="curl_span">{</span>' + right_side ;
 				$('#code_itself').html(result);
 				code_html=$('#code_itself').html();
 			} else if (code_html[i] == "}"){
 				var left_side = code_html.slice(0,i-1);
 				var right_side = code_html.slice(i+1, code_html.length);
-				var result = left_side + '<span class="red">}</span>' + right_side ;
+				//var result = left_side + '<span class="red" id="span' + i +'">}</span>' + right_side ;
+				var result = left_side + '<span class="red" class="curl_span">}</span>' + right_side ;
 				$('#code_itself').html(result);
 				code_html=$('#code_itself').html();
 			}
 		}
+		
 	}
 	highlight_brackets();
 	
-	$('#code_itself').on("keypress", function(){highlight_brackets()});
+	//I know it pushes the curser to the beggining of the line, but it CAN'T BE FIXED! D:
+	$('#code_itself').keyup(function(e){if (e.keyCode == 32){highlight_brackets();}});
 });
